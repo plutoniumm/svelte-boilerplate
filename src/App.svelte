2598c38077f17e42be03fbb0c69f10e9aa704d45
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-01-26 17:45:37 +0530

    Version Up

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script>
-	export let name;
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
@@ -15,16 +9,17 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
 </style>
+
+<main>
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
